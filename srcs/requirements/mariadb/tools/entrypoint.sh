8a510534c87cbdfebecb15134eddec3ddce33076
Peter Moelzer <moelzerpeter@gmail.com>
2025-12-05 17:22:28 +0100

    comments

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -39,6 +39,9 @@ done
 # Create database and user (idempotent - uses IF NOT EXISTS)
 echo "Setting up database and user..."
 mysql -u root <<EOF
+-- Set root password (fixes issue where root accepts any password)
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
